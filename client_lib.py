743ba34bcb89e9b6edd786770091c4b9d05c29a8
hoddr <hoddr@outlook.com>
2018-01-15 21:02:29 -0500

    Fixed errors on certain functions. Testing of task functions and docs to come. Data functionality almost completely tested.

--- client_lib.py
+++ client_lib.py
@@ -56,17 +56,22 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
-    # TODO returns an error for file.close() (str cannot be closed)
     def add_data_files(self, files, groupName='Not Applicable'):
         endpoint = self.url + '/data/upload'
         header = {'Authorization': 'Bearer ' + self.token}
+        data = {'groupName': groupName}
         input_files = []
         for file in files:
             print(file)
             input_files.append(('files', open(file, 'rb')))
         try:
             print(input_files)
-            res = requests.post(endpoint, headers=header, files=input_files)
+            res = requests.post(
+                endpoint,
+                headers=header,
+                files=input_files,
+                data=data)
+
             self.print_rsp(res)
         finally:
             for file in input_files:
@@ -74,14 +79,14 @@ class Voxel51API:
                 file[1].close()
             return res
 
-    # TODO Fix error on print res.json() for success
     def delete_file(self, fileID):
         endpoint = self.url + '/data/' + fileID
         header = {'Authorization': 'Bearer ' + self.token}
         res = requests.delete(endpoint, headers=header)
-        self.print_rsp(res)
+        pprint.pprint(res)
         return res
 
+    # TODO Finish testing this function
     def download_file(self, fileID):
         endpoint = self.url + '/data/' + fileID + '/download'
         header = {'Authorization': 'Bearer ' + self.token}
@@ -100,9 +105,10 @@ class Voxel51API:
         endpoint = self.url + '/data/group/' + groupName
         header = {'Authorization': 'Bearer ' + self.token}
         res = requests.delete(endpoint, headers=header)
-        self.print_rsp(res)
+        pprint.pprint(res)
         return res
 
+    # TODO Finish testing this function
     def download_data_set(self, groupName):
         endpoint = self.url + '/data/group/' + groupName + '/download'
         header = {'Authorization': 'Bearer ' + self.token}
@@ -112,6 +118,7 @@ class Voxel51API:
 
     # TASK FUNCTIONS
 
+    # TODO Test
     def tasks_page(self):
         endpoint = self.url + '/task'
         header = {'Authorization': 'Bearer ' + self.token}
@@ -119,9 +126,11 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
+    # TODO Test
     def create_task(self, file, params):
         pass
 
+    # TODO Test
     def get_current_tasks(self):
         endpoint = self.url + '/task/list'
         header = {'Authorization': 'Bearer ' + self.token}
@@ -129,6 +138,7 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
+    # TODO Test
     def run_task(self, taskID):
         endpoint = self.url + '/task/' + taskID + '/run'
         header = {'Authorization': 'Bearer ' + self.token}
@@ -136,6 +146,7 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
+    # TODO Test
     def pause_task(self, taskID):
         endpoint = self.url + '/task/' + taskID + '/pause'
         header = {'Authorization': 'Bearer ' + self.token}
@@ -143,6 +154,7 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
+    # TODO Test
     def stop_task(self, taskID):
         endpoint = self.url + '/task/' + taskID + '/stop'
         header = {'Authorization': 'Bearer ' + self.token}
@@ -150,6 +162,7 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
+    # TODO Test
     def change_task_status(self, taskID, status):
         if status != 'stop' or status != 'pause' or status != 'run':
             return 'IncorrectStatusError'
@@ -160,6 +173,7 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
+    # TODO Test
     def delete_task(self, taskID):
         endpoint = self.url + '/task/' + taskID
         header = {'Authorization': 'Bearer ' + self.token}
@@ -167,6 +181,7 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
+    # TODO Test
     def get_task_status(self, taskID):
         endpoint = self.url + '/task/' + taskID + '/status'
         header = {'Authorization': 'Bearer ' + self.token}
@@ -174,6 +189,7 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
+    # TODO Test
     def get_task_details(self, taskID):
         endpoint = self.url + '/task/' + taskID + '/specs'
         header = {'Authorization': 'Bearer ' + self.token}
@@ -181,6 +197,7 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
+    # TODO Test
     def get_task_output(self, taskID):
         endpoint = self.url + '/task/' + taskID + '/output'
         header = {'Authorization': 'Bearer ' + self.token}
@@ -197,6 +214,15 @@ class Voxel51API:
         self.print_rsp(res)
         return res
 
+    # TODO Test
+    def list_methods(self):
+        endpoint = self.url + '/process/list'
+        header = {'Authorization': 'Bearer ' + self.token}
+        res = requests.get(endpoint, headers=header)
+        self.print_res(res)
+        return res
+
+    # TODO Test
     def get_method_details(self, methodID):
         endpoint = self.url + '/process/' + methodID
         header = {'Authorization': 'Bearer ' + self.token}
@@ -210,3 +236,7 @@ class Voxel51API:
         pprint.pprint(res.url)
         pprint.pprint(res.status_code)
         pprint.pprint(res.json())
+
+    # TODO save to hidden file on local disk
+    def save_token(self):
+        pass
--- user_case.py
+++ user_case.py
@@ -6,19 +6,19 @@ api = Voxel51API()
 print('The following example demonstrates a typical user-case\n')
 print('Sign-up')
 api.signup('testCaseUser', 'admin')
-input('Press enter to continue...')
+input('Press enter to continue...\n')
 
 print('\nGet authentication token')
 api.authenticate('testCaseUser', 'admin')
-input('Press enter to continue...')
+input('Press enter to continue...\n')
 
 print('\nCheck data page for available methods')
 api.get_data_page()
-input('Press enter to continue...')
+input('Press enter to continue...\n')
 
 print('\nList all current data files on record')
 api.list_data_files()
-input('Press enter to continue...')
+input('Press enter to continue...\n')
 
 print('\nAs no data files are present, we need to upload some')
 file1 = os.getcwd() + '/use-case-data/ex1.txt'
@@ -27,22 +27,27 @@ file3 = os.getcwd() + '/use-case-data/ex3.jpeg'
 multiple_files = [file1, file2, file3]
 
 api.add_data_files(multiple_files, 'testData')
-input('Press enter to continue...')
+input('Press enter to continue...\n')
 
 print('\nNow check that the files exist')
 api.list_data_files()
-input('Press enter to continue...')
+input('Press enter to continue...\n')
 
 print('\nTo see specifics on one file, do the following')
 api.get_data_specs('1')
-input('Press enter to continue...')
+input('Press enter to continue...\n')
 
 print('\nCheck to make sure all the data files are there')
 api.list_data_files()
 
 print('\nGet the details of just one file')
 api.get_data_specs('1')
-input('Press enter to continue...')
+input('Press enter to continue...\n')
 
 print('\nDeleting a file you do not want anymore')
 api.delete_file('3')
+input('Press enter to continue...\n')
+
+print('\nGet remaining files by group name')
+api.get_data_set('testData')
+input('Press enter to continue...\n')
