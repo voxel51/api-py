368282ced4151bb4a07b3caf4135c50f662f396d
brimoor <brimoor@umich.edu>
2018-01-30 22:17:02 -0800

    removing editorial comments and following setup conventions established in ETA

--- setup.py
+++ setup.py
@@ -1,46 +1,26 @@
-from setuptools import setup
-from codecs import open
-from os import path
+#!/usr/bin/env python
+'''
+Install Voxel51 API Python client library.
 
-here = path.abspath(path.dirname(__file__))
+Copyright 2018, Voxel51, LLC
+voxel51.com
+'''
+from setuptools import setup, find_packages
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
-    name='voxel51-api',  # Required
-
-    version='1.0',  # Required
-
-    description='Python client library for accessing the Voxel51 ETA API.',
-
-    long_description=long_description,  # Optional
-
-    url='http://api.voxel51.com',  # Optional
-
-    author='Voxel51, LLC',  # Optional
-
-    author_email='david@voxel51.com',  # Optional
-
-    classifiers=[  # Optional
-        'Development Status :: 3 - Alpha',
-
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+    name="voxel51-api-python",
+    version="1.0",
+    description='Python client library for the Voxel51 Vision Services API',
+    author="Voxel51, LLC",
+    author_email="support@voxel51.com",
+    url="https://github.com/voxel51/api-python",
+    license="BSD 4-clause".
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
     ],
-
-    keywords='voxel51 api video analytics',  # Optional
-
-    py_modules=["voxel51/api"],
-
-    install_requires=['requests'],  # Optional
 )
