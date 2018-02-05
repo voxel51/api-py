7d460359371f7b4b818628f83a910e4401662539
brimoor <brimoor@umich.edu>
2018-02-04 22:34:16 -0800

    refactoring to formalize token as a Token class. Apply convention that token should be a JSON file

--- voxel51/auth.py
+++ voxel51/auth.py
@@ -7,72 +7,97 @@ Copyright 2017-2018, Voxel51, LLC
 voxel51.com
 
 David Hodgson, david@voxel51.com
+Brian Moore, brian@voxel51.com
 '''
-
+import logging
 import os
-LIB = os.path.dirname(os.__file__) + '/api-token.json'
-# Grabs the modules install location
+
+import voxel51.utils as voxu
 
 
-def load_token():
-    '''Searches for API authentication token.
-    Searches for the API token by checking if the VOXEL51_API_TOKEN
-    environment variable is set; if not, it searches for a
-    $LIB/.api-token.json file. Returns error if neither exist
-
-    Args:
-        None
-
-    Returns:
-        API token from environment variable or file
-    '''
-    token = os.environ.get('VOXEL51_API_TOKEN')
-    if (token is None):
-        return -1
-
-    else:
-        with open(LIB, 'rb') as f:
-            token = f.read()
-            if (token.length == 0):
-                return -2
-
-            else:
-                return token
+logger = logging.getLogger(__name__)
 
 
-def activate_token(token):
-    '''Copies input token to $LIB/.api-token.json.
+# Constants
+TOKEN_ENVIRON_VAR = "VOXEL51_API_TOKEN"
+VOXEL51_DIR = os.path.join(os.path.expanduser("~"), ".voxel51")
+TOKEN_PATH = os.path.join(VOXEL51_DIR, "api-token.json")
+PRIVATE_KEY_FIELD = "key"
+
+
+def activate_token(path):
+    '''Activates the given token by copying it to ~/.voxel51/api-token.json.
+
+    Subsequent voxel51.api.API() instances created will now use this token
+    for authentication.
 
     Args:
-        token (str): API token to store in $LIB/.api-token.json
-
-    Returns:
-        None
+        path (str): the path to an API token to store in $LIB/.api-token.json
     '''
-
-    try:
-        with open(LIB, 'w') as f:
-            f.write(token)
-
-    except IOError:
-        return -1
+    voxu.copy_file(path, TOKEN_PATH)
+    logger.info("Token successfully activated")
 
 
 def deactivate_token():
-    '''Deletes the stored token from $LIB/.api-token.json.
+    '''Deactivates (deletes) the currently active token, if any.
 
-    Args:
-        None
+    The active token is the token at ~/.voxel51/api-token.json
+    '''
+    try:
+        os.remove(TOKEN_PATH)
+        logger.info("Token '%s' successfully deactivated", TOKEN_PATH)
+    except OSError:
+        logger.info("No active token to deactivate")
+
+
+def load_token():
+    '''Loads the active API token.
+
+    If the VOXEL51_API_TOKEN environment variable is set, this is the active
+    token and will be loaded. Otherwise the token is loaded from
+    ~/.voxel51/api-token.json.
 
     Returns:
-        None
+        The active token, an instance of Token()
+
+    Raises:
+        TokenLoadError: if no valid token was found
     '''
-
+    path = os.environ.get(TOKEN_ENVIRON_VAR) or TOKEN_PATH
     try:
-        os.remove(LIB)
-
+        return Token.from_disk(path)
     except IOError:
-        return -1
+        raise TokenLoadError("No token found at '%s'", TOKEN_PATH)
 
 
-print(LIB)
+class TokenLoadError(Exception):
+    pass
+
+
+class Token(object):
+    '''A class encapsulating an API authentication token.'''
+
+    def __init__(self, content):
+        '''Creates a token object with the given contents
+
+        Args:
+            content (dict): a JSON dictionary defining an API token
+        '''
+        self._content = content
+
+    def get_header(self):
+        '''Returns a header dictionary for authenticating requests with
+        this token.'''
+        return {"Authorization": "Bearer " + self._content[PRIVATE_KEY_FIELD]}
+
+    @classmethod
+    def from_disk(cls, path):
+        '''Loads a token from disk.
+
+        Args:
+            path (str): the path to a valid token JSON file
+
+        Returns:
+            An instance of Token()
+        '''
+        return cls(voxu.read_json(path))
