b0fe7a28a7c3711950daf077ac83f02e33916132
brimoor <brimoor@umich.edu>
2018-03-16 07:08:38 -0700

    removing emails from public code

--- voxel51/api.py
+++ voxel51/api.py
@@ -4,9 +4,6 @@ Main Python interface for the Voxel51 Vision Services API.
 
 Copyright 2017-2018, Voxel51, LLC
 voxel51.com
-
-David Hodgson, david@voxel51.com
-Brian Moore, brian@voxel51.com
 '''
 import os
 
