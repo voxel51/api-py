f66f6fdc9ecf977de8cc656cf6382c4fef0bbed3
brimoor <brimoor@umich.edu>
2018-04-11 16:40:17 -0700

    adding Serializable base class

--- voxel51/utils.py
+++ voxel51/utils.py
@@ -61,3 +61,71 @@ def write_json(obj, path):
     ensure_basedir(path)
     with open(path, "wt") as f:
         f.write(json.dumps(obj, indent=4))
+
+
+class Serializable(object):
+    '''Base class for objects that can be serialized as JSON dictionaries.'''
+
+    def __str__(self):
+        return json.dumps(self.to_dict(), indent=4)
+
+    @classmethod
+    def from_dict(cls, d):
+        '''Constructs a Serializable object from a JSON dictionary. Subclasses
+        must implement this method.
+        '''
+        raise NotImplementedError("subclass must implement from_dict()")
+
+    @classmethod
+    def from_json(cls, path):
+        '''Constructs a Serializable object from a JSON file.
+
+        Subclasses may override this method, but, by default, this method
+        simply reads the JSON and calls from_dict(), which subclasses must
+        implement.
+        '''
+        return cls.from_dict(read_json(path))
+
+    def to_dict(self):
+        '''Serialzes the object into a JSON dictionary.
+
+        Returns:
+            a JSON serializable dictionary representation of this object.
+        '''
+        return {
+            v: _recurse(getattr(self, k))
+            for k, v in self._attributes().items()
+        }
+
+    def to_json(self, path):
+        '''Serialzes the object to disk.
+
+        Args:
+            path (str): the output JSON file path. The base output directory
+                is created, if necessary
+        '''
+        write_json(self.to_dict(), path)
+
+    def _attributes(self):
+        '''Returns a dictionary describing the class attributes to be
+        serialized.
+
+        Subclasses can override this method, but, by default, all attributes
+        in vars(self) are returned, minus private attributes (those starting
+        with "_").
+
+        Returns:
+            a dictionary mapping attribute names to field names (as they
+                should appear in the JSON file)
+        '''
+        return {a: a for a in vars(self) if not a.startswith("_")}
+
+
+def _recurse(v):
+    if isinstance(v, list):
+        return [_recurse(vi) for vi in v]
+    elif isinstance(v, dict):
+        return {ki: _recurse(vi) for ki, vi in v.items()}
+    elif isinstance(v, Serializable):
+        return v.to_dict()
+    return v
