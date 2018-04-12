663174ef33a1955456b6cbed42e50e09521c0099
brimoor <brimoor@umich.edu>
2018-04-11 18:35:11 -0700

    adding script to generate sphinx docs

---
+++ generate_docs.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Generate API documentation
+#
+# Usage:
+#  bash generate_docs.bash
+#
+# Copyright 2018, Voxel51, LLC
+#
+
+echo "**** Generating API documentation"
+
+sphinx-apidoc -f -o docs/source .
+
+cd docs
+make html
+cd ..
+
+echo "**** API documentation complete"
+
+# Open docs in browser
+open docs/build/html/index.html
