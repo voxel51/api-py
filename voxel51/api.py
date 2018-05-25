2191e97a14b3758fd3605412e1522f811f18b826
brimoor <brimoor@umich.edu>
2018-05-25 14:07:38 -0400

    removing spurious docstring

--- voxel51/api.py
+++ voxel51/api.py
@@ -32,10 +32,6 @@ class API(object):
          '''Starts a new API session.
 
         Args:
-            token_path: an optional path to a valid Token JSON file. If no
-            path is provided as an argument, it must instead be set via the
-            `VOXEL51_AGENT_TOKEN` environment variable
-
             token_path: an optional path to a valid Token JSON file. If no path
                 is provided as an argument, the ``VOXEL51_API_TOKEN``
                 environment variable is checked and, if set, the token is
