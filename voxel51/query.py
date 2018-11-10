ccd61693a9306fe3f703e4d2378e5cc5d7163849
brimoor <brimoor@umich.edu>
2018-11-09 23:50:35 -0500

    adding missing docstrings

--- voxel51/query.py
+++ voxel51/query.py
@@ -35,35 +35,88 @@ class BaseQuery(object):
         return self.to_string()
 
     def add_field(self, field):
+        '''Adds the given field to the query.
+
+        Args:
+            field (str): a query field to add
+
+        Returns:
+            the updated BaseQuery instance
+        '''
         if self._is_supported_field(field):
             self.fields.append(field)
         return self
 
     def add_fields(self, fields):
+        '''Adds the given fields to the query.
+
+        Args:
+            field (list): a list of query fields to add
+
+        Returns:
+            the updated BaseQuery instance
+        '''
         for field in fields:
             self.add_field(field)
         return self
 
     def add_all_fields(self):
+        '''Adds all supported fields to the query.
+
+        Returns:
+            the updated BaseQuery instance
+        '''
         this.add_fields(self._supported_fields)
         return self
 
     def add_search(self, field, search_str):
+        '''Adds the given search to the query.
+
+        Args:
+            field (str): the query field on which to search
+            search_str (str): the search string
+
+        Returns:
+            the updated BaseQuery instance
+        '''
         if self._is_supported_field(field):
             this.search.append("%s:%s" % (field, search_str))
         return self
 
     def sort_by(self, field, descending=True):
+        '''Adds the given search to the query.
+
+        Args:
+            field (str): the field on which to sort
+            descending (bool, optional): whether to sort in descending order
+
+        Returns:
+            the updated BaseQuery instance
+        '''
         if self._is_supported_field(field):
             self.sort = "%s:%s" % (field, "desc" if descending else "asc")
         return self
 
     def set_limit(self, limit):
+        '''Sets the record limit of the query.
+
+        Args:
+            limit (int): the desired record limit
+
+        Returns:
+            the updated BaseQuery instance
+        '''
         if isinstance(limit, int) and limit > 0:
             self.limit = limit
         return self
 
     def to_dict(self):
+        '''Converts the query instance into a dict suitable for passing to the
+        `requests` package.
+
+        Returns:
+            the query dict
+        '''
         obj = {}
         for key in vars(self):
             val = getattr(self, key)
@@ -72,6 +125,11 @@ class BaseQuery(object):
         return obj
 
     def to_string(self):
+        '''Converts the query instance into a string.
+
+        Returns:
+            the query string
+        '''
         return urlencode(self.to_dict())
 
     def _is_supported_field(self, field):
@@ -92,6 +150,7 @@ class AnalyticsQuery(BaseQuery):
     '''
 
     def __init__(self):
+        '''Initializes a AnalyticsQuery instance.'''
         super(AnalyticsQuery, self).__init__(
             ["id", "name", "description", "date"])
 
@@ -110,6 +169,7 @@ class DataQuery(BaseQuery):
     '''
 
     def __init__(self):
+        '''Initializes a DataQuery instance.'''
         super(DataQuery, self).__init__(
             ["id", "name", "encoding", "type", "size", "date", "expires"])
 
@@ -128,6 +188,7 @@ class JobsQuery(BaseQuery):
     '''
 
     def __init__(self):
+        '''Initializes a JobsQuery instance.'''
         super(JobsQuery, self).__init__(
             ["id", "name", "state", "archived", "date"])
 
