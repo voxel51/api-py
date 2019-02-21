81060707e6445fe5ec8c4581f6cd6b6c9a8a8d64
brimoor <brimoor@umich.edu>
2019-02-20 21:11:42 -0800

    supporting all_versions param for analytics query

--- voxel51/query.py
+++ voxel51/query.py
@@ -159,18 +159,34 @@ class AnalyticsQuery(BaseQuery):
 
     Attributes:
         fields (list): the list of fields to include in the returned records.
-            The supported query fields are `id`, `name`, `version`, `date`, and
-            `description`
+            The supported query fields are `id`, `name`, `version`,
+            `upload_date`, and `description`
         search (list): a list of `field:search_str` search strings to apply
         sort (str): a `field:asc/desc` string describing a sorting scheme
         offset (int): an offset index for the returned records list
         limit (int): the maximum number of records to return
+        all_versions (bool): whether to include all versions of each analytic
+            in the query response. By default, this is False
     '''
 
     def __init__(self):
-        '''Initializes a AnalyticsQuery instance.'''
+        '''Initializes an AnalyticsQuery instance.'''
         super(AnalyticsQuery, self).__init__(
             ["id", "name" "version", "upload_date", "description"])
+        self.all_versions = False
+
+    def set_all_versions(self, all_versions):
+        '''Sets the `all_versions` parameter of the query.
+
+        Args:
+            all_versions (bool): whether to include all versions of each
+                analytic in the query
+
+        Returns:
+            the updated AnalyticsQuery instance
+        '''
+        self.all_versions = all_versions
+        return self
 
 
 class DataQuery(BaseQuery):
