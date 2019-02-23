0b34589183448e87eaa9fd258a766715fbda7016
brimoor <brimoor@umich.edu>
2019-02-23 14:56:15 -0800

    adding future imports to ensure py2/py3 cross-compatibility

--- voxel51/jobs.py
+++ voxel51/jobs.py
@@ -4,7 +4,21 @@ API.
 
 | Copyright 2017-2019, Voxel51, Inc.
 | `voxel51.com <https://voxel51.com/>`_
+|
 '''
+# pragma pylint: disable=redefined-builtin
+# pragma pylint: disable=unused-wildcard-import
+# pragma pylint: disable=wildcard-import
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import *
+from future.utils import iteritems
+# pragma pylint: enable=redefined-builtin
+# pragma pylint: enable=unused-wildcard-import
+# pragma pylint: enable=wildcard-import
+
 import voxel51.utils as voxu
 
 
