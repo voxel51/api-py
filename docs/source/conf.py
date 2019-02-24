7cb90ceab286ffc6a9d02186f97e609e4e60eaf2
brimoor <brimoor@umich.edu>
2019-02-23 18:58:50 -0800

    supporting ability to correctly render relative links between .md files in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,7 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 source_suffix = [".rst", ".md"]
+m2r_parse_relative_links = True
 
 # The master toctree document.
 master_doc = "index"
