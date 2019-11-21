0aea72f5bab94a988caf1a63393b421074000897
brimoor <brimoor@umich.edu>
2019-11-21 08:59:29 -0500

    ensuring that logging is setup when cli package is used

--- voxel51/cli/__init__.py
+++ voxel51/cli/__init__.py
@@ -3,3 +3,20 @@
 | `voxel51.com <https://voxel51.com/>`_
 |
 '''
+# pragma pylint: disable=redefined-builtin
+# pragma pylint: disable=unused-wildcard-import
+# pragma pylint: disable=wildcard-import
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import *
+# pragma pylint: enable=redefined-builtin
+# pragma pylint: enable=unused-wildcard-import
+# pragma pylint: enable=wildcard-import
+
+import voxel51.users.logging as voxl
+
+
+# Ensure that logging is setup
+voxl.setup_logging()
